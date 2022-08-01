4ae807abfa18606c63134cf4f57747597d0291a8
Code 41 <washingtonbenton@gmail.com>
2022-08-01 13:30:26 -0400

    Server created

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 3001
+
+app.listen(PORT, () => {
+    console.log("Listening on PORT # https://localhost:3001");
+
+});
