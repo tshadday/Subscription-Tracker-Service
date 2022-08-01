ab3f1076df5c375550f4a1585d6d9d8ee7ddd9e6
Tanner Shadday <tshadday99@gmail.com>
2022-08-01 14:07:31 -0400

    merge fix

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
     console.log("Listening on PORT # https://localhost:3001");
