4172f5aa9d262eb8f6633bcf7eae3562b53bdefd
Code 41 <washingtonbenton@gmail.com>
2022-08-01 13:58:59 -0400

    updated server

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
+const path = require("path");
 const express = require("express");
+const sequelize = require("./config/connection");
+
+const model = require("./models");
 
 const app = express();
 
-const PORT = process.env.PORT || 3001
+
+
+
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
     console.log("Listening on PORT # https://localhost:3001");
