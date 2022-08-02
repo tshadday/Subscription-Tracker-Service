8393158c2ee617c730264e3b9b2d460bf662a29a
Alex Wilkinson <mrwilkinson1@gmail.com>
2022-08-02 13:42:17 -0400

    fixing merge

--- models/Subscription.js
+++ models/Subscription.js
@@ -1,8 +1,39 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes, Sequelize } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Subscription extends Model {}
+class Subscription extends Model { }
 
-Subscription.init()
+Subscription.init(
+    {
+        sub_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        sub_title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        cancel_date: {
+            type: DataTypes.DATEONLY,
+            allowNull: false,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'User',
+                key: 'user_id',
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "Subscription",
+    });
+
 
 module.exports = Subscription;
--- models/User.js
+++ models/User.js
@@ -1,8 +1,36 @@
 const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
-class User extends Model {}
+class User extends Model { }
 
-User.init()
+User.init(
+    {
+        user_id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [8]
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "User",
+    }
+);
 
 module.exports = User;
