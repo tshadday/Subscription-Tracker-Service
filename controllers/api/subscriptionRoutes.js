feb65e70ad32d15ace2354d69cadd93cf8468a0a
Tanner Shadday <tshadday99@gmail.com>
2022-08-04 16:15:36 -0400

    fixed js files and various other minor details

---
+++ controllers/api/subscriptionRoutes.js
@@ -0,0 +1,89 @@
+const router = require('express').Router();   
+const { User } = require('../../models/User');
+const { Subscription } = require('../../models/Subscription');
+const { Unsub } = require('../../models/Unsub');
+const auth = require('../../utils/auth');
+
+router.post('/', auth, async (req, res) => {
+    try {
+        const newSub = await Subscription.create({
+          ...req.body,
+          user_id: req.session.user_id,
+        });
+    
+        res.status(200).json(newSub);
+      } catch (err) {
+        res.status(400).json(err);
+      }
+});
+
+router.delete('/:id', auth, async (req, res) => {
+    try {
+      const subData = await Subscription.destroy({
+        where: {
+          id: req.params.id,
+          user_id: req.session.user_id,
+        },
+      });
+  
+      if (!subData) {
+        res.status(404).json({ message: 'No subscription found with this id' });
+        return;
+      }
+  
+      res.status(200).json(projectData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+  // GET all subscriptions
+  router.get('/:user_id', (req, res) => {
+    Subscription.findAll().then((subData) => {
+      res.json(subData);
+    });
+  });
+  
+  // UPDATE a subscription
+  router.put('/:sub_id', (req, res) => {
+    Subscription.update(
+      {
+        sub_name: req.body.sub_title,
+        cancel_date: req.body.cancel_date,
+      },
+      {
+        where: {
+          sub_id: req.params.sub_id,
+        },
+      }
+    )
+      .then((updatedSub) => {
+        res.json(updatedSub);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.json(err);
+      });
+  });
+  
+  // POST how to unsubscribe links
+  router.get('/sub/:id', async (req, res) => {
+    try {
+      const unsubData = await Unsub.findbyPk(req.params.id);
+  
+      if(!unsubData) {
+        res.status(404).json({message: "No subscriptions with this id"});
+        return;
+      }
+  
+      // Serialize unsub data
+      const unsub = unsubData.get({ plain: true })
+      // render serialized "unsub" data to "subscription" handlebars page
+      res.render('subscription', unsub);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+
+  module.exports = router;
--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,8 +1,24 @@
 const router = require('express').Router();   
-const { User, update } = require('../../models/User');
+const { User } = require('../../models/User');
 const { Subscription } = require('../../models/Subscription');
 const { Unsub } = require('../../models/Unsub');
 
+router.post('/', async (req, res) => {
+  try {
+    const userData = await User.create(req.body);
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.status(200).json(userData);
+    })
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// route: api/users/login
 router.post('/login', async (req, res) => {
   try {
     // takes email input from user and saves as userEmail
@@ -46,78 +62,5 @@ router.post('/logout', (req, res) => {
     res.status(404).end();
   }
 });
-
-// CREATE subscription
-router.post('/', (req, res) => {
-  Subscription.create(req.body)
-    .then((newSub) => {
-      res.json(newSub);
-    })
-    .catch((err) => {
-      res.json(err);
-    });
-});
-
-// GET all subscriptions
-router.get('/:user_id', (req, res) => {
-  Subscription.findAll().then((subData) => {
-    res.json(subData);
-  });
-});
-
-// UPDATE a subscription
-router.put('/:sub_id', (req, res) => {
-  Subscription.update(
-    {
-      sub_name: req.body.sub_title,
-      cancel_date: req.body.cancel_date,
-    },
-    {
-      where: {
-        sub_id: req.params.sub_id,
-      },
-    }
-  )
-    .then((updatedSub) => {
-      res.json(updatedSub);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.json(err);
-    });
-});
-
-// DELETE a subscription
-router.delete('/:sub_id', (req, res) => {
-  Subscription.destroy({
-    where: {
-      sub_id: req.params.sub_id,
-    },
-  })
-    .then((deletedSub) => {
-      res.json(deletedSub);
-    })
-    .catch((err) => res.json(err));
-});
-
-// POST how to unsubscribe links
-router.get('/sub/:id', async (req, res) => {
-  try {
-    const unsubData = await Unsub.findbyPk(req.params.id);
-
-    if(!unsubData) {
-      res.status(404).json({message: "No subscriptions with this id"});
-      return;
-    }
-
-    // Serialize unsub data
-    const unsub = unsubData.get({ plain: true })
-    // render serialized "unsub" data to "subscription" handlebars page
-    res.render('subscription', unsub);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
 
 module.exports = router;
